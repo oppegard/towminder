689bf956cec69184399c0665eee59c494eea182a
oppegard <oppegard@gmail.com>
2009-05-30 14:39:30 -0600

    Added mobile companies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :mobile_companies
+
+  map.resources :remindees
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
