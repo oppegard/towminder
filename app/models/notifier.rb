016ae474d24eb8790b0e6b56376b9a0d6fdb5251
oppegard <oppegard@gmail.com>
2009-06-30 22:28:15 -0600

    Change to get notifier working

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,8 +3,8 @@ class Notifier < ActionMailer::Base
 
   def notify(remindees, alert_period="hour")
     #subject    'Test Reminder'
-    recipients 'glenn@towminder.com'
-    bcc         remindees
+    recipients  remindees
+    #bcc         remindees
     from       'glenn@towminder.com'
     sent_on    Time.now
     
