e2363c7e3b5c734deedb12ae2551eb49ce814e3a
oppegard <oppegard@gmail.com>
2009-07-10 05:31:02 -0600

    Added reply-to email header to Notifier model

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,9 +4,10 @@ class Notifier < ActionMailer::Base
   def notify(remindee_emails, alert_period="hour")
     recipients  'glenn@towminder.com'
     bcc         remindee_emails
-    from       'glenn@towminder.com'
-    sent_on    Time.now
-    body       :alert_period => alert_period
+    from        'glenn@towminder.com'
+    reply_to    'glenn@towminder.com'
+    sent_on     Time.now
+    body        :alert_period => alert_period
   end
 
 end
